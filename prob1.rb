a914c36883bf8841827fc2d8e1152da0bcc5d35a
aldafari <aldafari@memphis.edu>
2024-04-24 00:43:28 -0500

    Done with prob1

--- prob1.rb
+++ prob1.rb
@@ -1,1 +1,9 @@
-hello
+require 'httparty'
+def prob1
+
+  api_key = '88b76204ade06e8f4226860413d41dfb'
+
+  response = HTTParty.get("https://pro.openweathermap.org/data/2.5/forecast/climate?lat=35.117500&lon=-89.971107.&appid=88b76204ade06e8f4226860413d41dfb")
+
+end
+puts prob1
